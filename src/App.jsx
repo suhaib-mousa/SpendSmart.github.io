84cf98afc2ace22d958d538edba428eace6a3eeb
ebtihal55 <83011174+ebtihal55@users.noreply.github.com>
2025-05-16 20:04:50 +0300

    Set up React pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Budget from './pages/Budget';
+import Planner from './pages/Planner';
+import Discounts from './pages/Discounts';
+import Tips from './pages/Tips';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 import './styles/global.css';
 
 function App() {
@@ -10,7 +17,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Other routes will be added here */}
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/discounts" element={<Discounts />} />
+        <Route path="/tips" element={<Tips />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
