6dd2e555e11c527d51988917098f047c75e9c040
Ebtihal Smadi <83011174+ebtihal55@users.noreply.github.com>
2025-05-26 17:45:09 +0300

    Fix Jest configuration for ES modules

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
